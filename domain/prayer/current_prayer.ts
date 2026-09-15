b9a8f76108ef4f685fa4181fadf25dfacaca3b5a
“rihlahidali” <rihlahidali@gmail.com>
2026-09-15 23:34:52 +0800

    update date-fns directory

--- domain/prayer/current_prayer.ts
+++ domain/prayer/current_prayer.ts
@@ -7,7 +7,7 @@ import {
   isBefore,
   subSeconds,
 } from "date-fns";
-import ms from "date-fns/locale/ms";
+import ms from "date-fns/locale/ms/index.js";
 import type { PrayerClock, TimedPrayer } from "./prayer";
 
 const CORE_PRAYER_COUNT = 8;
